8492ec81f5639424d962645679b009959ee9c049
Davis Yang <davis.yang@aevoe.com>
2017-12-26 10:17:46 +0800

    加入專案檔案。

---
+++ UnitOfWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitOfWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
